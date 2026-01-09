6a1db2ff91895dbbb9ef9c3ec7e9b38a44c2ac40
Udondiane <65195365+Udondiane@users.noreply.github.com>
2026-01-09 21:13:01 +0000

    Create layout.tsx

---
+++ components/ui/components/ui/components/ui/components/ui/app/partner/layout.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { Navigation } from '@/components/ui/Navigation';
+
+export default function PartnerLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen">
+      <Navigation 
+        userType="partner" 
+        partnerType="employment"
+        userName="Partner User"
+        orgName="Your Organisation"
+      />
+      <main className="flex-1 p-8 bg-gray-50">
+        {children}
+      </main>
+    </div>
+  );
+}
